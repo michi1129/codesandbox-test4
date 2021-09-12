50f9b8f739a0bfbd20066e1fe6af7cab892f6e87
ro <ro.michi1129@gmail.com>
2021-09-12 19:44:49 +0900

    edit src

--- src/index.js
+++ src/index.js
@@ -1,34 +1,70 @@
 import "./styles.css";
 
-const addOnClick = () => {
-  const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
 
+const createIncompleteListItem = (text) => {
   // li
   const li = document.createElement("li");
 
   //div
   const div = document.createElement("div");
   div.className = "list-row";
-  div.innerText = inputText;
+
+  //span
+  const span = document.createElement("span");
+  span.innerText = text;
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("add");
+    const deleteTarget = completeButton.parentNode.parentNode;
+    deleteFromIncompleteList(deleteTarget);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻る";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteListItem(text);
+    });
+
+    addTarget.appendChild(span);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget.parentNode);
   });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("delete");
+    const deleteTarget = completeButton.parentNode.parentNode;
+    deleteFromIncompleteList(deleteTarget);
   });
 
+  div.appendChild(span);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
   li.appendChild(div);
 
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+const addOnClick = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompleteListItem(inputText);
 };
 
 document
